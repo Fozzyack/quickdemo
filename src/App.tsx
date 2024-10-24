3a7badae6a64aca7f880ed1afe074ffd117b1194
Frasier <fsundra@gmail.com>
2024-10-24 16:15:38 +0800

    added responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Welcome from "./components/Welcome";
 
 function App() {
 	return (
-		<div className="bg-gradient-to-br from-lime-200 to-70% to-sky-300 flex flex-col justify-center items-center h-screen">
+		<div className="bg-gradient-to-br from-lime-200 to-70% to-sky-300 flex flex-col justify-center items-center min-h-screen pt-8 md:pt-0">
 			<Welcome />
             <Links />
 		</div>
